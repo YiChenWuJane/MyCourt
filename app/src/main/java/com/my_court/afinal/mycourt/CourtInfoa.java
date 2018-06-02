df5d82400802ed74fe2678024476240cb7cf1cdf
YiChenWuJane <lc007274@gmail.com>
2018-06-02 17:56:52 +0800

    已經可以用MQTT把資料即時給APP了！

--- app/src/main/java/com/my_court/afinal/mycourt/CourtInfoa.java
+++ app/src/main/java/com/my_court/afinal/mycourt/CourtInfoa.java
@@ -2,12 +2,25 @@ package com.my_court.afinal.mycourt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import helpers.MqttHelper;
 
 public class CourtInfoa extends AppCompatActivity {
 
     private Button btn_return;
+    private TextView tv_court_a_d1;
+    MqttHelper mqttHelper;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +33,38 @@ public class CourtInfoa extends AppCompatActivity {
                 finish();
             }
         });
+        tv_court_a_d1 = findViewById(R.id.CourtAData1);
+        try {
+            startMqtt();
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startMqtt() throws MqttException {
+        mqttHelper = new MqttHelper(getApplicationContext());
+        mqttHelper.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean b, String s) {
+
+            }
+
+            @Override
+            public void connectionLost(Throwable throwable) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                Log.w("Debug", mqttMessage.toString());
+                tv_court_a_d1.setText(mqttMessage.toString());
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+        });
+
     }
 }
